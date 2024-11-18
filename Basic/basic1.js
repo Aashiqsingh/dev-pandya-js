95c0c4356e08a834e32669f5a52d4f178b58e944
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-11-18 10:52:04 +0530

    basic

---
+++ Basic/basic1.js
@@ -0,0 +1,35 @@
+// console.log("hello world!");
+// console.info("information function..");
+// console.warn("warning function..");
+// console.error("error function..");
+
+// datatype = number , string , boolean , undefined , object
+
+a = 10;
+console.log("value of a = ",a);
+console.log("type of a = ",typeof(a));
+
+b = 12.23;
+console.log("value of b = ",b);
+console.log("type of b = ",typeof(b));
+
+c = "krishn";
+console.log("value of c = ",c);
+console.log("type of c =",typeof c);
+
+isAvctive = true;
+console.log("value of isAvctive = ",isAvctive);
+console.log("type of isAvctive = ",typeof isAvctive);
+
+var d;
+console.log("value of d = ",d);
+console.log("type of d = ",typeof d);
+
+var x = null;
+console.log("value of x = ",x);
+console.log("type of x = ",typeof x);
+
+
+
+
+
---
+++ Basic/basic2.js
@@ -0,0 +1,48 @@
+// var a = 10;
+// console.log("outer block value of a =",a);
+
+// let b = 20;
+// console.log("outer block value of b =",b);
+
+
+// {
+//     console.log("inner block value of a = ",a);
+//     console.log("outer block value of b = ",b);
+    
+//     var c = 40;
+//     console.log("inner block value of c = ",c);
+
+//     let d = 50;
+//     console.log("inner block value of d = ",d);
+    
+    
+    
+// }
+
+// console.log("outer block value of a = ",c);
+
+// console.log("outer block value of d = ",d);
+
+// var : Redeclare , reassign 
+// var a = 10;
+// console.log("value of a = ",a);
+
+// var a = 20;
+// console.log("value of a = ",a);
+
+// let : reassign
+
+// let a = 10;
+// console.log("value of a = ",a);
+
+// a = 20;
+// console.log("value of a = ",a);
+
+
+// const : not possible redeclare  and reassign
+// const pi = 3.14;
+// console.log("value of pi = ",pi);
+
+// pi = 3.15;
+// console.log("value of pi = ",pi);
+
