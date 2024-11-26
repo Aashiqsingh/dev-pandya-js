7a77f55f000d610d1ba7ca50249cbd2db6fde260
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-11-26 10:38:16 +0530

    string

---
+++ String/string1.js
@@ -0,0 +1,88 @@
+var str = "     aoyal technosoft pvt ltd.         ";
+console.log(str.length);
+
+
+// var x = str.trim()
+// var x = str.trimEnd()
+// console.log(x.length);
+
+
+
+
+// var x = str.split("o")
+// var x = str.split(" ")
+// var x = str.split("")
+
+
+// console.log(x);
+
+// for(let i=0;i<x.length;i++){
+    
+//     console.log(x[i].toUpperCase());
+    
+// }
+
+
+
+// var x = str.slice(1)
+// var x = str.slice(1,4)
+// var x = str.slice(-4)
+// var x = str.slice(-4,-1)
+
+
+
+// console.log(x);
+
+
+
+
+// var x = str.replace("a","R")
+// var x = str.replace(" ","-")
+// var x = str.replace("pvt","private")
+// var x = str.replaceAll(" ","-")
+
+
+// console.log(x);
+
+// var x = str.repeat(12)
+// console.log(x);
+
+
+
+// var x = str.indexOf(" ")
+// var x = str.indexOf("technosoft")
+
+// console.log(x);
+
+
+
+// var x = str.includes("tech")
+// var x = str.includes("ft")
+
+// console.log(x);
+
+
+
+
+// var str2 = " dev pandya"
+
+// console.log(str.length);
+
+// var x = str.charAt(10)
+// console.log(x);
+
+// var x = str.charCodeAt(1)
+// console.log(x);
+
+// str.codePointAt
+
+// var x = str2.concat(str)
+// console.log(x);
+
+// var x = str + str2 
+// console.log(x);
+
+// var x = str.endsWith("td.")
+// var x = str.startsWith("b")
+// console.log(x);
+
