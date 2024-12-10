f0dab303653dc126fb548e920de0a6be335cf0ec
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-12-10 10:47:08 +0530

    array

---
+++ Array/Array1.js
@@ -0,0 +1,48 @@
+// Array : collection of data 
+
+// Array : hyrarchical 
+
+// var arr = ["ram","seeta",12,7,true,5.4];
+
+// // console.log(arr);
+// // // console.log(arr.length);
+
+// // console.log(arr[0]);
+
+// // console.log(arr[1]);
+
+// for(let i=0;i<arr.length;i++){
+//     console.log(arr[i]);
+    
+// }
+
+
+// var arr = [23,45,64,24,56,22,45];
+// let min = arr[0];
+
+// for(let i=1;i<arr.length;i++){
+
+//     if(min > arr[i])
+//     {
+//         min = arr[i];
+//     }
+// }
+
+
+// console.log(min);
+
+
+
+
+
+
+// var sum = 0;
+// for(let i=0;i<arr.length;i++){
+
+//     // console.log(arr[i]);
+//     // sum = sum + arr[i]
+//     sum += arr[i]
+    
+// }
+
+// console.log(sum);
---
+++ Array/Array2.js
@@ -0,0 +1,85 @@
+var arr = ["ram","seeta","geeta","krishna","lakshman","rukmani","radha"];
+
+
+// var newArray = arr.slice(2)
+// var newArray = arr.slice(2,4)
+// var newArray = arr.slice(-4,-2)
+
+
+// console.log(newArray);
+
+// arr.splice(2,0,"hanuman")
+// arr.splice(2,2,"hanuman")
+// arr.splice(3,1,"hanuman","krishna","radha")
+
+
+// console.log(arr);
+
+
+
+
+
+
+
+
+
+// var x = arr.reverse()
+// console.log(x);
+
+
+
+// var x = arr.includes("geeta")
+// console.log(x);
+
+// var x = arr.indexOf("geeta")
+// console.log(x);
+
+// var x = arr.join(" ")
+// var x = arr.join("-")
+// var x = arr.join("$")
+// var x = arr.join("")
+
+
+
+// console.log(x);
+
+
+
+
+
+
+
+
+
+
+// var arr2 = [2,3,4,5,6,]
+// console.log(arr.length);
+
+// var newArray = arr2.concat(arr)
+// console.log(newArray);
+
+// var x = arr.push("rahul")
+// arr.push("rahul","samir","tejas")
+// console.log(arr);
+// console.log(x);
+
+
+// var x = arr.pop()
+// console.log(x);
+// console.log(arr);
+
+
+// arr.unshift("rahul","tejas")
+// console.log(arr);
+
+// var x = arr.shift()
+// console.log(x);
+
+// console.log(arr);
+
+
+// var str = "devpandya";
+
+// var x = str.split("").reverse().join("")
+// console.log(x);
+
